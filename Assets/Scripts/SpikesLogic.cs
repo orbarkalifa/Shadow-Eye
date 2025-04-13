136766d31778f8511528da8c1e21a53997a2ef5d
Or Bar Califa <orbarkalifa@gmail.com>
2025-04-14 02:44:36 +0300

    implemented spikes logic

---
+++ Assets/Scripts/SpikesLogic.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpikesLogic : MonoBehaviour
+{
+    public Transform resetPosition;
+
+    [SerializeField] private LayerMask playerLayer;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (((1 << other.gameObject.layer) & playerLayer) != 0)
+        {
+            var player = other.GetComponent<MainCharacter>();
+            if (player != null)
+            {
+                Debug.Log("SPIKED");
+                player.transform.position = resetPosition.position;
+                player.TakeDamage(1);
+            }
+        }
+    }
+
+
+    private void ResetPlayer(MainCharacter player)
+    {
+        player.TakeDamage(1);
+        Debug.Log("SPIKED");
+        player.transform.position = resetPosition.position;
+    }
+}
