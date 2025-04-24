15b3e06a82e800732934de85364613c0d8f6c292
Daniel Fradkin <dtselon@gmail.com>
2025-04-24 22:50:54 +0300

    fixed reapting cunsome toturial

--- Assets/Scripts/ConsumeUnlock.cs
+++ Assets/Scripts/ConsumeUnlock.cs
@@ -7,10 +7,12 @@ public class ConsumeUnlock : MonoBehaviour
     [SerializeField] private string tutorialMessage = "Wall Grab Unlocked!\nHold toward wall + Jump to cling!";
     [SerializeField] private float duration = 3f;
     [SerializeField] private TutorialPanelController tutorialPanel;
+    private bool triggered;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (triggered||!other.CompareTag("Player")) return;
 
         var player = other.GetComponent<MainCharacter>();
         if (player != null)
@@ -19,6 +21,7 @@ public class ConsumeUnlock : MonoBehaviour
             if (GameStateManagement.GSManager.Instance.tutorialsEnabled)
             {
                 tutorialPanel.ShowMessage(tutorialMessage, duration);
+                triggered = true;
             }
         }
     }
--- Assets/Scripts/TutorialTrigger.cs
+++ Assets/Scripts/TutorialTrigger.cs
@@ -5,7 +5,6 @@ public class TutorialTrigger : MonoBehaviour
     [SerializeField] private string tutorialMessage;
     [SerializeField] private float duration = 3f;
     [SerializeField] private TutorialPanelController tutorialPanel;
-
     private bool triggered;
 
     private void OnTriggerEnter2D(Collider2D other)
