8021b626685cce4b84ac73ea3a79256bf346fa6a
orbarkalifa <orbarkalifa@gmail.com>
2025-05-11 20:42:31 +0300

    merge

--- Assets/Scripts/Suits/Abilities/DashMovement.cs
+++ Assets/Scripts/Suits/Abilities/DashMovement.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 
 public class DashMovement : SuitAbility
 {
-    private void Awake()
-    {
-        cooldownTime = 1.2f;
-    }
-
     public override void ExecuteAbility(GameObject character)
     {
         CharacterMovement movement = character.GetComponent<CharacterMovement>();
